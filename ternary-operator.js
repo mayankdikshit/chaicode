b0f6347769e5ea829074a3fea6304ac88dfacf5c
mayankdikshit <mayankdikshit711@gmail.com>
2025-03-02 21:45:14 +0530

    added: ternary operator

---
+++ ternary-operator.js
@@ -0,0 +1,14 @@
+let age = 19;
+
+if (age>=18){
+    console.log("can drive");
+    
+} else {
+    console.log("underage");
+    
+}
+
+let yourAge = (age>=18) ? console.log("you can drive") : console.log("you're a kid");
+
+
+let 
