5357339ff5745f5893e43285279e3399d106fae5
mayankdikshit <mayankdikshit711@gmail.com>
2025-03-29 11:28:02 +0530

    new folder

---
+++ basics/Execution-context.js
@@ -0,0 +1,25 @@
+debugger
+
+sayHi()
+
+const userName = "mayank"
+const userAge = 25;
+
+function sayHi(){
+    const a = 14;
+    const b = 12;
+    console.log(a, b);
+    add(7,9);    
+}
+
+function add(x, y){
+    sayMyName()
+    return x+y;
+}
+
+function sayMyName(){
+    console.log('mayank');
+    
+}
+
+console.log('Program Ended');
---
+++ basics/foreach.js
@@ -0,0 +1,14 @@
+const fruits = ['banana', 'apple', 'peach', 'mango', 'grapes'];
+
+// for (let fruit of fruits) {
+//     console.log(fruit);
+    
+// }
+
+// console.log(fruit);
+
+
+
+fruits.forEach((fruit) => {
+    console.log(fruit)
+});
