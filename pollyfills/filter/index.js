93b8d2f0074cea1f9a841e362961c35f07f501de
mayankdikshit <mayankdikshit711@gmail.com>
2025-03-29 13:12:34 +0530

    added filter

---
+++ pollyfills/filter/index.js
@@ -0,0 +1,21 @@
+if (!Array.prototype.myFilter){
+    Array.prototype.myFilter = function(userFn){
+        const result= []
+
+        for (let i = 0; i <this.length; i++){
+            if (userFn(this[i])){
+                result.push(this[i])
+            }
+        }
+
+        return result;
+
+    }
+}
+
+
+const arr = [1,2,3,4,5,6]
+
+const n = arr.myFilter((e)=>e%2==0)
+
+console.log(n)
